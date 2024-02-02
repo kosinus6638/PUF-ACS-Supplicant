62dc84e53311d402a027e8c0896de7c6af809dfe
Felix Meyer <felix.meyer@hs-osnabrueck.de>
2024-02-02 15:36:26 +0100

    Do not sign up, send tagged frames

--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,16 @@ int send_tagged_frame(bool initial_frame = false) {
     static uint8_t hk_mac[32];
     static uint8_t k_mac[36];
     static Payload p;
-    static int counter = 0;
+
+    static PUF_Performance pp;
 
     memset(k_mac, 0, sizeof(k_mac));
     size_t k_offset = 0;
 
     // Copy MAC or last hash into buffer
     if(initial_frame) {
+        pp.src_mac = s.mac;
+        pp.dst_mac = switch_mac;
         k_offset = sizeof(s.mac.bytes);
         memcpy(k_mac, s.mac.bytes, k_offset);
     } else {
@@ -59,18 +62,9 @@ int send_tagged_frame(bool initial_frame = false) {
     p.load1 = *(reinterpret_cast<uint16_t*>(hk_mac));
     p.load2 = *(reinterpret_cast<uint16_t*>(hk_mac+30));
 
-
-    PUF_Performance pp;
-    pp.src_mac = s.mac;
-    pp.dst_mac = switch_mac;
     pp.calc();
     pp.set_payload(p);
-
-    printf("%d Sending payload: 0x%04x 0x%04x\n", counter++, pp.get_payload().load1, pp.get_payload().load2);
-
     net.send(pp.binary(), pp.header_len());
-
-    s.mac.hash(1);
 
     return 0;
 }
@@ -88,13 +82,14 @@ extern "C" void app_main() {
 
     try {
         s.init();
-        s.sign_up();
+        // s.sign_up();
         s.connect();
     } catch(const Exception& e) {
         puts(e.what());
     }
 
     printf("%s\n", s.connected() ? "Connected" : "Connection failed" );
+    send_tagged_frame(true);
 
     while(true) {
         loop();
@@ -105,6 +100,6 @@ extern "C" void app_main() {
 void loop() {
     // Error insufficient TX buffer size:   (208) esp_eth/src/esp_eth_mac_esp.c: emac_esp32_transmit
     // Calls function: hal/emac_hal:        (407) emac_hal_transmit_frame
-    sys_delay_ms(500);
-    // send_tagged_frame();
+    sys_delay_ms(16);
+    send_tagged_frame();
 }
