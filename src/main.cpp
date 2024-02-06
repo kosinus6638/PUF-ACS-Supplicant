4673616d586640f6cf055a5e80592c50c681baee
Felix Meyer <felix.meyer@hs-osnabrueck.de>
2024-02-06 14:14:38 +0100

    Added 'send_tagged_frame'

--- src/main.cpp
+++ src/main.cpp
@@ -32,38 +32,43 @@ puf::Supplicant s(net, sram_puf);
 
 int send_tagged_frame(bool initial_frame = false) {
     using namespace puf;
-    static uint8_t hk_mac[32];
-    static uint8_t k_mac[36];
-    static Payload p;
 
+    static uint8_t hk_mac[32];
+    static uint8_t concat_buf[36];
+    static VLAN_Payload p;
     static PUF_Performance pp;
 
-    memset(k_mac, 0, sizeof(k_mac));
     size_t k_offset = 0;
 
-    // Copy MAC or last hash into buffer
     if(initial_frame) {
+        memset(hk_mac, 0, sizeof(hk_mac));
+
+        // Build static frame on first send
         pp.src_mac = s.mac;
         pp.dst_mac = switch_mac;
+        pp.calc();
+
+        // Copy MAC into concatenation buffer
         k_offset = sizeof(s.mac.bytes);
-        memcpy(k_mac, s.mac.bytes, k_offset);
+        memcpy(concat_buf, s.mac.bytes, k_offset);
     } else {
+        // Copy last hk_mac into concatenation buffer
         k_offset = sizeof(hk_mac);
-        memcpy(k_mac, hk_mac, k_offset);
+        memcpy(concat_buf, hk_mac, k_offset);
     }
 
-    // Concatenate with k
-    memcpy(k_mac+k_offset, (void*)s.k.private_p, 4);
+    // Concatenate 4 digits of k to concatenation buffer
+    memcpy(concat_buf+k_offset, (void*)s.k.private_p, 4);
 
-    if( mbedtls_sha256(k_mac, sizeof(k_mac), hk_mac, 0) != ESP_OK) {
+    if( mbedtls_sha256(concat_buf, sizeof(concat_buf), hk_mac, 0) != ESP_OK) {
         return 1;
     }
 
+    // Set VLAN tags
     p.load1 = *(reinterpret_cast<uint16_t*>(hk_mac));
     p.load2 = *(reinterpret_cast<uint16_t*>(hk_mac+30));
-
-    pp.calc();
     pp.set_payload(p);
+
     net.send(pp.binary(), pp.header_len());
 
     return 0;
@@ -100,6 +105,7 @@ extern "C" void app_main() {
 void loop() {
     // Error insufficient TX buffer size:   (208) esp_eth/src/esp_eth_mac_esp.c: emac_esp32_transmit
     // Calls function: hal/emac_hal:        (407) emac_hal_transmit_frame
-    sys_delay_ms(16);
+    sys_delay_ms(5000);
+    puts("Sending frame");
     send_tagged_frame();
 }
