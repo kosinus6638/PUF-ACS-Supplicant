f97f5439c54f81686d0b4573a223b0ff34e7cadb
Felix Meyer <felix.meyer@hs-osnabrueck.de>
2024-02-09 17:49:51 +0100

    Moved to newer commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 
 #include <lwip/sockets.h>
-
-#include <mbedtls/sha256.h>
+#include <driver/uart.h>
+#include <esp_vfs_dev.h>
 
 #include "supplicant.h"
 #include "packets.h"
@@ -15,8 +15,10 @@
 #include "SRAM_PUF.h"
 #include "Ethernet_Network.h"
 
+#include <rom/ets_sys.h>
 
-#define delay_us(us) esp_rom_delay_us(us)
+// #define delay_us(us) esp_rom_delay_us(us)
+#define delay_us(us) ets_delay_us(us)
 
 TaskHandle_t main_tsk_hdl;
 eth_frame eth_buf;
@@ -30,52 +32,11 @@ EthernetNetwork net;
 puf::Supplicant s(net, sram_puf);
 
 
-int send_tagged_frame(bool initial_frame = false) {
-    using namespace puf;
-
-    static uint8_t hk_mac[32];
-    static uint8_t concat_buf[36];
-    static VLAN_Payload p;
-    static PUF_Performance pp;
-
-    size_t k_offset = 0;
-
-    if(initial_frame) {
-        memset(hk_mac, 0, sizeof(hk_mac));
-
-        // Build static frame on first send
-        pp.src_mac = s.mac;
-        pp.dst_mac = switch_mac;
-        pp.calc();
-
-        // Copy MAC into concatenation buffer
-        k_offset = sizeof(s.mac.bytes);
-        memcpy(concat_buf, s.mac.bytes, k_offset);
-    } else {
-        // Copy last hk_mac into concatenation buffer
-        k_offset = sizeof(hk_mac);
-        memcpy(concat_buf, hk_mac, k_offset);
-    }
-
-    // Concatenate 4 digits of k to concatenation buffer
-    memcpy(concat_buf+k_offset, (void*)s.k.private_p, 4);
-
-    if( mbedtls_sha256(concat_buf, sizeof(concat_buf), hk_mac, 0) != ESP_OK) {
-        return 1;
-    }
-
-    // Set VLAN tags
-    p.load1 = *(reinterpret_cast<uint16_t*>(hk_mac));
-    p.load2 = *(reinterpret_cast<uint16_t*>(hk_mac+30));
-    pp.set_payload(p);
-
-    net.send(pp.binary(), pp.header_len());
-
-    return 0;
-}
-
-
 void loop();
+
+
+unsigned long net_cpu_time_used = 0;
+unsigned long delay_cpu_time_used = 0;
 
 
 extern "C" void app_main() {
@@ -85,6 +46,13 @@ extern "C" void app_main() {
     eth_buf.len = 0;
     main_tsk_hdl = xTaskGetCurrentTaskHandle();
 
+
+    /* Install UART driver for interrupt-driven reads and writes */
+    uart_driver_install(CONFIG_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0);
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);    // This causes everything to be slower
+
     try {
         s.init();
         // s.sign_up();
@@ -93,19 +61,25 @@ extern "C" void app_main() {
         puts(e.what());
     }
 
-    printf("%s\n", s.connected() ? "Connected" : "Connection failed" );
-    send_tagged_frame(true);
+    int ctr = 0;
 
-    while(true) {
-        loop();
+    if(s.connected()) {
+        s.transmit(NULL, 0, true);
+
+        while(ctr < 500000) {
+            loop();
+            ++ctr;
+        }
     }
 }
 
 
 void loop() {
+
+    static char msg[] = "Hello World";
     // Error insufficient TX buffer size:   (208) esp_eth/src/esp_eth_mac_esp.c: emac_esp32_transmit
     // Calls function: hal/emac_hal:        (407) emac_hal_transmit_frame
-    sys_delay_ms(5000);
-    puts("Sending frame");
-    send_tagged_frame();
+
+    delay_us(20000);   // As of now 90 is the minimum
+    s.transmit(reinterpret_cast<uint8_t*>(msg), sizeof(msg));
 }
