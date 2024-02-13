148c1651a885031ae32dad39d073b05329e3abf1
Felix Meyer <felix.meyer@hs-osnabrueck.de>
2024-02-13 14:51:59 +0100

    Main loop now waits for instructions

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <time.h>
 
-
-#include <lwip/sockets.h>
-#include <driver/uart.h>
-#include <esp_vfs_dev.h>
-
 #include "supplicant.h"
 #include "packets.h"
 #include "platform.h"
@@ -14,29 +9,56 @@
 
 #include "SRAM_PUF.h"
 #include "Ethernet_Network.h"
+#include "Serial_Slave.h"
 
 #include <rom/ets_sys.h>
+#include <esp_timer.h>
 
 // #define delay_us(us) esp_rom_delay_us(us)
 #define delay_us(us) ets_delay_us(us)
 
+
 TaskHandle_t main_tsk_hdl;
 eth_frame eth_buf;
-
-
-const static puf::MAC switch_mac = {0x04, 0x92, 0x26, 0x87, 0x84, 0x11};
+int global_err = 0;
+SerialOptions opts;
 
 
 SRAM_PUF sram_puf;
 EthernetNetwork net;
 puf::Supplicant s(net, sram_puf);
 
+bool keepGoing = true;
 
-void loop();
+
+void timer_isr(void *args) {
+    keepGoing = false;
+}
 
 
-unsigned long net_cpu_time_used = 0;
-unsigned long delay_cpu_time_used = 0;
+void speedtest() {
+    const esp_timer_create_args_t timer_args = {
+        .callback = &timer_isr,
+        .arg = NULL,
+        .name = "one-shot"
+    };
+
+    esp_timer_handle_t timer;
+    int duration = opts.speedtest_seconds * 1000*1000;
+    esp_timer_create(&timer_args, &timer);
+
+    char msg[] = "Hello World";
+    keepGoing = true;
+
+    esp_timer_start_once(timer, duration);   // Start for 5 seconds
+    s.transmit(NULL, 0, true);  // First frame is a special case
+    while(keepGoing) {
+        s.transmit( reinterpret_cast<uint8_t*>(msg), sizeof(msg) );
+        delay_us(opts.delay_us);
+    }
+
+    keepGoing = true;
+}
 
 
 extern "C" void app_main() {
@@ -46,40 +68,44 @@ extern "C" void app_main() {
     eth_buf.len = 0;
     main_tsk_hdl = xTaskGetCurrentTaskHandle();
 
-
-    /* Install UART driver for interrupt-driven reads and writes */
-    uart_driver_install(CONFIG_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0);
-
-    /* Tell VFS to use UART driver */
-    esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);    // This causes everything to be slower
-
     try {
         s.init();
-        // s.sign_up();
-        s.connect();
-    } catch(const Exception& e) {
+    } catch(const Exception &e) {
         puts(e.what());
     }
 
-    int ctr = 0;
+    /* Start UART task on second core */
+    xTaskCreatePinnedToCore(&cmd_task, "cmd_task", 4096, NULL, 5, NULL, APP_CPU_NUM);
 
-    if(s.connected()) {
-        s.transmit(NULL, 0, true);
+    uint32_t ulNotifiedValue = 0;
 
-        while(ctr < 500000) {
-            loop();
-            ++ctr;
+    while(true) {
+        xTaskNotifyWait( 0x00, ULONG_MAX, &ulNotifiedValue, portMAX_DELAY );
+        switch(ulNotifiedValue) {
+            case CMD::REGISTER:
+                try {
+                    s.sign_up();
+                } catch(const Exception &e) {
+                    global_err = 1;
+                }
+                break;
+            case CMD::CONNECT:
+                try {
+                    s.connect();
+                } catch(const Exception &e) {
+                    global_err = 2;
+                }
+                break;
+            case CMD::SPEEDTEST:
+                if( !s.connected() ) {
+                    global_err = 3;
+                    break;
+                }
+                speedtest();
+                break;
+
+            default:
+                ;
         }
     }
-}
-
-
-void loop() {
-
-    static char msg[] = "Hello World";
-    // Error insufficient TX buffer size:   (208) esp_eth/src/esp_eth_mac_esp.c: emac_esp32_transmit
-    // Calls function: hal/emac_hal:        (407) emac_hal_transmit_frame
-
-    delay_us(20000);   // As of now 90 is the minimum
-    s.transmit(reinterpret_cast<uint8_t*>(msg), sizeof(msg));
 }
